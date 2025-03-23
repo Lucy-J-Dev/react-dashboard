fc81e905f7e3620230ce4bf3bff69f61e164b919
Lucy Jiménez <lucy.jimenez.mejia@gmail.com>
2025-03-22 19:15:02 -0500

    Instalación de tailwinds, creación de componentes Header y Sidebar, adición de constantes e imagenes base

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+
 const App = () => {
-  return <div>App</div>;
+  const [darkMode, setDarkMode] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div className={`${darkMode && "dark"} font-quickSand`}>
+      <Header toggleDarkMode={toggleDarkMode} darkMode={darkMode} toggleSidebar={toggleSidebar} />
+      <Sidebar isSidebarOpen={isSidebarOpen} />
+    </div>
+  );
 };
 
 export default App;
